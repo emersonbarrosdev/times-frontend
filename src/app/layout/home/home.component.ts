4e8f4149805aa331adcb2c06819e1bd080b1a15f
Emerson Barros <emerson.sbarros3@gmail.com.com>
2024-07-10 09:34:04 -0300

    feat: implementa lógica, HTML e CSS do componente Home com navegação para a tela de login

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
 
+  constructor(private router: Router) { }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
